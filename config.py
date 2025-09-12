811e743492f5c97f2887ee17f8ae25fa3607279b
Brin84 <brin1984@mail.ru>
2025-09-12 16:55:58 +0300

    интеграция AI

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AI_TOKEN=os.getenv("AI_TOKEN")
---
+++ settings_ai/settings.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+import asyncio
+from config import AI_TOKEN
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=AI_TOKEN,
+)
+
+
+async def ask(prompt: str):
+    """корутина для асинхронного запроса к OpenRouter"""
+    completion = await asyncio.to_thread(
+        client.chat.completions.create,
+        model="deepseek/deepseek-chat",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return completion.choices[0].message.content
+
+
+async def main_test():
+    response = await ask("Как дела, мой друг?")
+    print(response)
+
+
+if __name__ == "__main__":
+    asyncio.run(main_test())
